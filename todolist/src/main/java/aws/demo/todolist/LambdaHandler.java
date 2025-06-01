b43b7db772e00846610865acf11d5dcd0e07e59b
conghau7620-ptit <conghau7620.ptit@gmail.com>
2025-06-01 17:27:51 +0700

    update config Lambda

--- todolist/src/main/java/aws/demo/todolist/LambdaHandler.java
+++ todolist/src/main/java/aws/demo/todolist/LambdaHandler.java
@@ -4,29 +4,54 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.springframework.context.annotation.Configuration;
-
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
-import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
-@Configuration(proxyBeanMethods = false)
-public class LambdaHandler implements RequestStreamHandler {
-    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+//public class LambdaHandler implements RequestHandler<String, String> {
+//    private static final ApplicationContext context;
+//
+//    static {
+//        try {
+//            context = SpringApplication.run(TodolistApplication.class);
+//        } catch (Exception e) {
+//            // Log the error for debugging
+//            System.err.println("Failed to initialize Spring context: " + e.getMessage());
+//            e.printStackTrace();
+//            throw new RuntimeException("Spring context initialization failed", e);
+//        }
+//    }
+//
+//    @Override
+//    public String handleRequest(String input, Context lambdaContext) {
+//        lambdaContext.getLogger().log("Input: " + input); 
+//        // Use Spring beans if needed
+//        // MyService service = context.getBean(MyService.class);
+//        return "Hello from Lambda: " + input;
+//    }
+//}
 
-    static {
-        try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(TodolistApplication.class);
-        } catch (ContainerInitializationException e) {
-            throw new RuntimeException("Could not initialize Spring Boot application", e);
-        }
-    }
+public class LambdaHandler implements RequestStreamHandler {
+
+    private static final SpringBootProxyHandlerBuilder<AwsProxyRequest> handlerBuilder =
+        new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
+            .defaultProxy()
+            .asyncInit()
+            .springBootApplication(TodolistApplication.class);
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-        handler.proxyStream(inputStream, outputStream, context);
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+        throws IOException {
+        try {
+			handlerBuilder.build().proxyStream(inputStream, outputStream, context);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ContainerInitializationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
